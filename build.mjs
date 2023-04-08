33be048e54ae3f4888bf4eee40dc7962c6cfdc59
Jack Matthews <jm5112356@gmail.com>
2023-04-08 15:12:12 -0400

    Import latest build script and init CI

--- build.mjs
+++ build.mjs
@@ -1,11 +1,49 @@
-import { readFile, writeFile } from "fs/promises";
+import { readFile, writeFile, readdir } from "fs/promises";
+import { extname } from "path";
 import { createHash } from "crypto";
 
 import { rollup } from "rollup";
-import { swc } from "rollup-plugin-swc3";
 import esbuild from "rollup-plugin-esbuild";
 import commonjs from "@rollup/plugin-commonjs";
 import nodeResolve from "@rollup/plugin-node-resolve";
+import swc from "@swc/core";
+
+const extensions = [".js", ".jsx", ".mjs", ".ts", ".tsx", ".cts", ".mts"];
+
+/** @type import("rollup").InputPluginOption */
+const plugins = [
+    nodeResolve(),
+    commonjs(),
+    {
+        name: "swc",
+        async transform(code, id) {
+            const ext = extname(id);
+            if (!extensions.includes(ext)) return null;
+
+            const ts = ext.includes("ts");
+            const tsx = ts ? ext.endsWith("x") : undefined;
+            const jsx = !ts ? ext.endsWith("x") : undefined;
+
+            const result = await swc.transform(code, {
+                filename: id,
+                jsc: {
+                    externalHelpers: true,
+                    parser: {
+                        syntax: ts ? "typescript" : "ecmascript",
+                        tsx,
+                        jsx,
+                    },
+                },
+                env: {
+                    targets: "defaults",
+                    include: ["transform-classes", "transform-arrow-functions"],
+                },
+            });
+            return result.code;
+        },
+    },
+    esbuild({ minify: true }),
+];
 
 const manifest = JSON.parse(await readFile(`./manifest.json`));
 const outPath = `./dist/index.js`;
@@ -14,26 +52,14 @@ try {
     const bundle = await rollup({
         input: `./${manifest.main}`,
         onwarn: () => { },
-        plugins: [
-            nodeResolve(),
-            commonjs(),
-            esbuild({ minify: true }),
-            swc({
-                env: {
-                    targets: "defaults",
-                    include: [
-                        "transform-classes",
-                        "transform-arrow-functions",
-                    ],
-                },
-            }),
-        ],
+        plugins,
     });
 
     await bundle.write({
         file: outPath,
         globals(id) {
-            if (id.startsWith("@vendetta")) return id.substring(1).replace(/\//g, ".");
+            if (id.startsWith("@vendetta"))
+                return id.substring(1).replace(/\//g, ".");
             const map = {
                 react: "window.React",
             };
